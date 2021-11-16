e786712750608158f12c6c1787cbaee0f90efcc4
mortal <seregagl1996@gmail.com>
2021-11-16 12:13:10 +0300

    remove main.css

--- #src/js/index.js
+++ #src/js/index.js
@@ -1,8 +1,9 @@
 @@include('components/ibg.js')
 @@include('components/block-hidden.js')
 @@include('components/header.js')
-@@include('components/document-click.js')
+/* @@include('components/document-click.js') */
 @@include('components/slider.js')
 @@include('components/scroll-shadows.js')
 
 /* @@include('components/media-query.js') */
+/*  */
